40dd86323e7392b7677ea2104de9240c2570dcae
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-05-05 12:57:02 +0530

    Pointer to class Members

---
+++ C-Plus-Plus/BookPrograms/ClassAndObjects/pointerToClassMembers.cpp
@@ -0,0 +1,36 @@
+/* Pointer to class Members */
+
+#include<iostream>
+using namespace std;
+
+class cl{
+	public:
+		int val; 
+		cl(int i)
+		{
+			val = i;
+		}
+		int double_val()
+		{
+			return val + val;
+		}
+};
+
+int main()
+{
+	int cl :: *data ;	// data member pointer
+	int (cl ::*func)();	// function member pointer 
+	cl ob1(1),ob2(2);	// create objects
+	
+	data =  &cl:: val;	// get offset of val
+	func = &cl :: double_val;	// get offset of double_val()
+	
+	cout << "Here are the given values: ";
+	cout<< ob1.*data<< " " << ob2.*data<< "\n";
+	
+	cout<< "Here they are doubled: ";
+	cout<< (ob1.*func)() << " ";
+	cout<< (ob2.*func)()<< "\n";
+	
+	return 0;
+}
