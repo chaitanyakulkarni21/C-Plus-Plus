8ca2dee3c45366378129b16bf0d7625f19e890c4
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-05-05 11:08:44 +0530

    Arrays of objects 2

---
+++ C-Plus-Plus/BookPrograms/ClassAndObjects/ArrayOfObjects/ArrayOfObjects2.cpp
@@ -0,0 +1,28 @@
+/* Arrays of Objects 2 */ 
+
+#include<iostream>
+using namespace std;
+
+class cl{
+	int i;
+	public: 
+		cl(int j)
+		{
+			i = j;		//Constructor
+		}
+		int get_i()
+		{
+			return i;
+		}
+};
+
+int main()
+{
+	cl ob[3] = {1,2,3};		// initializers
+	int i;
+	
+	for (i = 0 ; i < 3 ; i++)
+	{
+		cout << ob[i].get_i()<<"\n";
+	}
+}
