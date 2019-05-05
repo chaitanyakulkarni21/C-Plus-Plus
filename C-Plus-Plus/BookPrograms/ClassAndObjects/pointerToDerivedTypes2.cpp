b6a221b41353a0946cb60a9fda23ab44320daec9
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-05-05 13:15:12 +0530

    Pointer to Class Members 2

---
+++ C-Plus-Plus/BookPrograms/ClassAndObjects/pointerToDerivedTypes2.cpp
@@ -0,0 +1,42 @@
+/* Pointer to Class Members 2*/
+
+#include<iostream>
+using namespace std;
+
+class cl{
+	public:
+		cl(int i)
+		{
+			val = i;
+		}
+		int val;
+		int double_val()
+		{
+			return val + val;
+		}
+};
+
+int main()
+{
+	int cl :: *data;	// data member pointer
+	int (cl :: *func)();	//function member pointer
+	cl ob1(1),ob2(2);	// create objects
+	
+	cl *p1, *p2;
+	
+	p1 = &ob1;	// access objects through pointer
+	p2 = &ob2;
+	
+	data = &cl :: val;	// get offset of val
+	func = &cl :: double_val;	// get offset of double_val()
+	
+	cout<< "Here are the values: ";
+	cout<< p1->*data << " "<< p2->*data << "\n";
+	
+	cout << "Here they are doubled ";
+	
+	cout << (p1->*func)()<< " ";
+	cout << (p2->*func)()<< " \n";
+	
+	return 0;
+}
